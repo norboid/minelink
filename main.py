4b5143f99e608a28cf977ae61e199beb8d4a0d10
norboid <67892369+norboid@users.noreply.github.com>
2025-04-20 18:07:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ async def on_message(message):
 
                 embed = discord.Embed(
                     title="📥 Code Received",
-                    description=f"**User:** {message.author.mention}\n**Code:** {message.content}",
+                    description=f"**User:** {message.author.mention}\n**Code:** `{message.content}`",
                     color=discord.Color.blue()
                 )
 
