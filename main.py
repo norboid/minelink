3f194a34edebef978f9a8beba0404bbc72e79292
norboid <67892369+norboid@users.noreply.github.com>
2025-04-20 23:37:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-TOKEN = "your_token_here"
+TOKEN = "DISCORD_TOKEN"
 MOD_CHANNEL_ID = 1362997933552959558
 intents = discord.Intents.default()
 intents.message_content = True
